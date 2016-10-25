9a9c12eed1836e7dd4ba336d87cf86e35a7a5120
Don Reamey <dreame>
2016-10-24 23:48:48 -0700

    clean up code, remove unused stuff

--- src/com/juliar/codegenerator/CodeGenerator.java
+++ src/com/juliar/codegenerator/CodeGenerator.java
@@ -1,24 +1,10 @@
 package com.juliar.codegenerator;
 
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.net.URL;
-import java.util.Enumeration;
 import java.util.List;
-
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
-import org.objectweb.asm.*;
 import org.objectweb.asm.commons.*;
-import org.objectweb.asm.commons.Method;
-
-import static com.sun.org.apache.bcel.internal.generic.InstructionConstants.ISTORE_1;
-import static com.sun.org.apache.bcel.internal.generic.InstructionConstants.ISTORE_2;
-import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Node;
 import static java.lang.System.out;
 import static org.objectweb.asm.Opcodes.*;
 import com.juliar.nodes.*;
@@ -46,7 +32,7 @@ public class CodeGenerator {
         mw.visitEnd();
 
 
-// creates a MethodWriter for the 'main' method
+        // creates a MethodWriter for the 'main' method
         mw = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
         // pushes the 'out' field (of type PrintStream) of the System class
         mw.visitFieldInsn(GETSTATIC, "java/lang/System", "out","Ljava/io/PrintStream;");
@@ -86,39 +72,6 @@ public class CodeGenerator {
         FileOutputStream fos = new FileOutputStream("JuliarFuture.class");
         fos.write(code);
         fos.close();
-
-
-        // ------------------------------------------------------------------------
-        // Same example with a GeneratorAdapter (more convenient but slower)
-        // ------------------------------------------------------------------------
-/*
-        cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-        cw.visit(V1_1, ACC_PUBLIC, "Example", null, "java/lang/Object", null);
-
-        // creates a GeneratorAdapter for the (implicit) constructor
-        Method m = Method.getMethod("void <init> ()");
-        GeneratorAdapter mg = new GeneratorAdapter(ACC_PUBLIC, m, null, null,
-                cw);
-        mg.loadThis();
-        mg.invokeConstructor(Type.getType(Object.class), m);
-        mg.returnValue();
-        mg.endMethod();
-
-        // creates a GeneratorAdapter for the 'main' method
-        m = Method.getMethod("void main (String[])");
-        mg = new GeneratorAdapter(ACC_PUBLIC + ACC_STATIC, m, null, null, cw);
-        mg.getStatic(Type.getType(System.class), "out",
-                Type.getType(PrintStream.class));
-        mg.push("Hello world!");
-        mg.invokeVirtual(Type.getType(PrintStream.class),
-                Method.getMethod("void println (String)"));
-        mg.returnValue();
-        mg.endMethod();
-
-        cw.visitEnd();
-
-        code = cw.toByteArray();
-       */
     }
 
     private MethodVisitor EvaluateExpressions(Node root, MethodVisitor mw, GeneratorAdapter ga, Integer stackSize ){
@@ -163,6 +116,7 @@ public class CodeGenerator {
                 out.println(left + right);
             }
         }
-return mw;
+
+        return mw;
     }
 }
