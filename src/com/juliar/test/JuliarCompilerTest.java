5f60151daffb73fbf5f0a4a113c5e4eb80e65a6a
Don <don.reamey@hotmail.com>
2016-10-26 17:36:08 -0700

    Add test class and class loader

---
+++ src/com/juliar/test/JuliarCompilerTest.java
@@ -0,0 +1,43 @@
+package com.juliar.test;
+
+import com.juliar.JuliarCompiler;
+import junit.framework.TestCase;
+
+import java.lang.reflect.Method;
+
+import static java.lang.System.out;
+
+/**
+ * Created by donreamey on 10/25/16.
+ */
+public class JuliarCompilerTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testCompile() throws Exception {
+        try {
+            JuliarCompiler compiler = new JuliarCompiler();
+            compiler.compile("test.jrl", ".");
+            JuliarClassLoader classLoader = new JuliarClassLoader();
+
+            Class <?> juliar = classLoader.findClass("JuliarFuture");
+
+            if (juliar != null) {
+                Method m = juliar.getMethod("juliarMethod");
+                Object obj = juliar.newInstance();
+                if (m != null && obj != null) {
+                    m.invoke(obj, null);
+                }
+            }
+        }
+        catch(Exception ex) {
+            out.println(ex.getMessage());
+        }
+    }
+}
