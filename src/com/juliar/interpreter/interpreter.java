14a3b8c1efc6a4a8b533c95ce2fe99a1a21e6f0b
Don <don.reamey@hotmail.com>
2017-01-08 22:26:40 -0800

    working on a quick interpreter. This allows validation of the language faster than generating byte code

---
+++ src/com/juliar/interpreter/interpreter.java
@@ -0,0 +1,7 @@
+package com.juliar.interpreter;
+
+/**
+ * Created by donreamey on 1/8/17.
+ */
+public class interpreter {
+}
---
+++ src/com/juliar/nodes/AssignmentNode.java
@@ -0,0 +1,7 @@
+package com.juliar.nodes;
+
+/**
+ * Created by donreamey on 1/7/17.
+ */
+public class AssignmentNode {
+}
