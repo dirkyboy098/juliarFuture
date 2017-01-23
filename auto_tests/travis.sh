2adb029e12b410f3acca968168a633323b235b21
TheAndreiM <andreifundrei@gmail.com>
2017-01-22 23:14:15 -0500

    Test 7

--- auto_tests/travis.sh
+++ auto_tests/travis.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "a) Downloading"
-echo wget -O idea.tar.gz https://data.services.jetbrains.com/products/download?code=IIC&platform=linuxWithoutJDK 
+wget -O idea.tar.gz https://data.services.jetbrains.com/products/download?code=IIC&platform=linuxWithoutJDK &
+wait
 echo "b) Extracting"
 tar zxf idea.tar.gz
 echo "c) Removing"
