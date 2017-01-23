91e1a682517fed3d8749e87dd9e70c9840fd3ce7
TheAndreiM <andreifundrei@gmail.com>
2017-01-22 22:58:07 -0500

    Test 4

--- auto_tests/build-idea.sh
+++ auto_tests/build-idea.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ ! -d ./idea-IC ]; then
-    wget https://data.services.jetbrains.com/products/download?code=IIC&platform=linuxWithoutJDK
-    tar zxf ideaIC-*.tar.gz
-    rm -rf ideaIC-*.tar.gz
+echo "a) Downloading"
+wget https://data.services.jetbrains.com/products/download?code=IIC&platform=linuxWithoutJDK
+echo "b) Extracting"
+tar zxf ideaIC-*.tar.gz
+echo "c) Removing"
+rm -rf ideaIC-*.tar.gz
 
-    # Move the versioned IDEA folder to a known location
-    ideaPath=$(find . -name 'idea-IC*' | head -n 1)
-    mv ${ideaPath} ./idea-IC
-fi
+# Move the versioned IDEA folder to a known location
+ideaPath=$(find . -name 'idea-IC*' | head -n 1)
+echo "D) Moving $ideaPath"
+mv ${ideaPath} ./idea-IC
