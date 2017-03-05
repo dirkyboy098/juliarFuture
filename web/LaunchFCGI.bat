7e4bd266481e5ac6571619d4d0b5199d8858da86
TheAndreiM <andreifundrei@gmail.com>
2017-03-05 10:43:33 -0500

    LaunchFCGI JuliarCompiler.jar detection

--- web/LaunchFCGI.bat
+++ web/LaunchFCGI.bat
@@ -1,7 +1,16 @@
 taskkill /im java.exe /F
 taskkill /im nginx.exe /F
 
+set "mycurrdir=%cd%"
+
 cd ..
+if not exist JuliarCompiler.jar (
+    echo "ERROR: File JuliarCompiler.jar is not found!!! Cannot Launch!"
+    timeout /t 15
+    Exit 2
+)
 start cmd.exe /C java -jar -DFCGI_PORT=9000 JuliarCompiler.jar
 cd C:\nginx\
 start nginx.exe
+cd %mycurrdir%
+
