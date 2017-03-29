2a1ccd4132dd3513bba5bd7fb673770f85eb799c
Don Reamey <don.reamey@outlook.com>
2017-03-28 21:45:20 -0700

    got file open working. Need to store the results somewhere.

--- src/com/juliar/interpreter/EvaluatePrimitives.java
+++ src/com/juliar/interpreter/EvaluatePrimitives.java
@@ -1,6 +1,7 @@
 package com.juliar.interpreter;
 
 import com.juliar.nodes.*;
+import sun.jvm.hotspot.utilities.Assert;
 
 /**
  * Created by donreamey on 3/28/17.
@@ -10,32 +11,41 @@ public class EvaluatePrimitives {
         String functionName = ((FinalNode)n.getInstructions().get(0)).dataString();
         Node argumentNode = n.getInstructions().get(2);
 
-        if (functionName.equals( "printLine")) {
-            if (argumentNode instanceof VariableNode) {
-                VariableNode variableName = (VariableNode) argumentNode;
-                Object variable = activationFrame.variableSet.get(variableName.variableName);
+        printLine(activationFrame, functionName, argumentNode);
 
-                if (variable != null && variable instanceof PrimitiveNode) {
-                    PrimitiveNode primitiveNode = (PrimitiveNode) variable;
-                    FinalNode finalNode = (FinalNode) primitiveNode.getInstructions().get(0);
-                    com.juliar.pal.Primitives.sys_print_line(finalNode.dataString());
-                    return;
-                }
-            }
+        fileOpen(functionName, argumentNode);
+    }
 
-            if (argumentNode instanceof IntegralTypeNode) {
-                FinalNode finalNode = (FinalNode) argumentNode.getInstructions().get(0);
-                com.juliar.pal.Primitives.sys_print_line(finalNode.dataString());
-                return;
-            }
-        }
-
+    private static void fileOpen(String functionName, Node argumentNode) {
         if (functionName.equals( "fileOpen")){
             if (argumentNode instanceof FinalNode) {
                 FinalNode finalNode = (FinalNode) argumentNode;
                 com.juliar.pal.Primitives.sys_file_open(finalNode.dataString());
                 return;
             }
+        }
+    }
+
+    private static void printLine(ActivationFrame activationFrame, String functionName, Node argumentNode) {
+        if (functionName.equals( "printLine")) {
+            FinalNode finalNode = null;
+
+            if (argumentNode instanceof VariableNode) {
+                VariableNode variableName = (VariableNode) argumentNode;
+                Object variable = activationFrame.variableSet.get(variableName.variableName);
+
+                if (variable != null && variable instanceof PrimitiveNode) {
+                    PrimitiveNode primitiveNode = (PrimitiveNode) variable;
+                    finalNode = (FinalNode) primitiveNode.getInstructions().get(0);
+                }
+            }
+
+            if (argumentNode instanceof IntegralTypeNode) {
+                finalNode = (FinalNode) argumentNode.getInstructions().get(0);
+            }
+
+            assert finalNode != null;
+            com.juliar.pal.Primitives.sys_print_line(finalNode.dataString());
         }
     }
 
