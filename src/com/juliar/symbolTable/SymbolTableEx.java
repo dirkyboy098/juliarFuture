4475b93910073702fd3b16c97d519a4ea020e821
Don Reamey <don.reamey@outlook.com>
2017-04-09 17:16:05 -0700

    working on symbol table

---
+++ src/com/juliar/symbolTable/SymbolTableEx.java
@@ -0,0 +1,14 @@
+package com.juliar.symbolTable;
+
+import com.juliar.nodes.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by don on 4/8/17.
+ */
+public class SymbolTableEx {
+    public String levelName;
+    public List<SymbolTableEx> scope = new ArrayList<>();
+}
