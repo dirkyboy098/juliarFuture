7045fc51f019b7ef0859e18ca556da540dd24c43
Don <don.reamey@hotmail.com>
2017-04-11 14:43:16 -0700

    fixed the && check

--- src/com/juliar/pal/Primitives.java
+++ src/com/juliar/pal/Primitives.java
@@ -70,7 +70,7 @@ public class Primitives {
     }
 
     public static String stripQuotes( String s){
-        if (s.startsWith("\"") && s.startsWith("\"")) {
+        if (s.startsWith("\"") && s.endsWith("\"")) {
             return (s.substring(1, s.length()-1));
         }
 
