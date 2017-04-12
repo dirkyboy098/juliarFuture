5c21d1f92b148c60fd29c74feda753c3654d666b
TheAndreiM <andreifundrei@gmail.com>
2017-04-11 22:31:20 -0400

    Primitives support for ` and '

--- src/com/juliar/pal/Primitives.java
+++ src/com/juliar/pal/Primitives.java
@@ -70,7 +70,9 @@ public class Primitives {
     }
 
     public static String stripQuotes( String s){
-        if (s.startsWith("\"") && s.endsWith("\"")) {
+        if (s.startsWith("\"") && s.endsWith("\"") ||
+                s.startsWith("'") && s.endsWith("'") ||
+                s.startsWith("`") && s.endsWith("`")) {
             return (s.substring(1, s.length()-1));
         }
 
