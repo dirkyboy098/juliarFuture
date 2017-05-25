d53ccc40d71eec3e25df17006911432ea479c686
TheAndreiM <andreifundrei@gmail.com>
2017-05-24 21:43:03 -0400

    Updater update

--- src/com/juliar/gui/Updater.java
+++ src/com/juliar/gui/Updater.java
@@ -1,9 +1,63 @@
 package com.juliar.gui;
 
+import com.juliar.errors.LogMessage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
 /**
  * Created by AndreiM on 5/22/2017.
  */
 public class Updater {
+    public Updater(){
+        Scanner scanner = null;
+        String version = "";
+        try {
+            scanner = new Scanner(new URL("https://juliar.org/version.txt").openStream(), "UTF-8");
+            scanner.useDelimiter("\\A");
+            version = scanner.next();
+        } catch (IOException e) {
+            LogMessage.message("Failed to access Juliar Website");
+        }
+        if(!version.equals(checksum())){
+            /*TODO DOWNLOAD NEW VERSION OF JAR FILE*/
+        }
+    }
 
+    public StringBuilder checksum(){
+        File currentJavaJarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+        String filepath = currentJavaJarFile.getAbsolutePath();
+        StringBuilder sb = new StringBuilder();
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");// MD5
+            FileInputStream fis = new FileInputStream(filepath);
+            byte[] dataBytes = new byte[1024];
+            int nread = 0;
+
+            while((nread = fis.read(dataBytes)) != -1)
+                md.update(dataBytes, 0, nread);
+
+            byte[] mdbytes = md.digest();
+
+            for(int i=0; i<mdbytes.length; i++)
+                sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100 , 16).substring(1));
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return sb;
+    }
 
 }
