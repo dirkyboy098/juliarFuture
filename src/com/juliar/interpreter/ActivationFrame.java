53aa034d41d04c15be689fd742dc5b8207ae9d6c
Don <don.reamey@hotmail.com>
2017-09-10 20:01:17 -0700

    interpreter changes

--- src/com/juliar/interpreter/ActivationFrame.java
+++ src/com/juliar/interpreter/ActivationFrame.java
@@ -2,15 +2,16 @@ package com.juliar.interpreter;
 
 import com.juliar.nodes.Node;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Stack;
 
 /**
  * Created by donreamey on 1/18/17.
  */
 public class ActivationFrame {
     public String frameName;
-    public Map<String, Node> variableSet = new HashMap<>();
-    public Deque<Node> parameterStack = new ArrayDeque<>();
-    public Deque<Node> operandStack = new ArrayDeque<>();
+    public HashMap<String, Node> variableSet = new HashMap<>();
+    public Stack<Node> parameterStack = new Stack<>();
+    public Stack<Node> operandStack = new Stack<>();
     public Node returnNode;
 }
