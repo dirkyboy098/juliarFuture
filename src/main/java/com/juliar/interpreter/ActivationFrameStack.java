f70ac8a77a56d39cd18cae23f65b40b071625772
donreamey <Don.Reamey@hotmail.com>
2017-11-03 16:25:57 -0700

    working

--- src/main/java/com/juliar/interpreter/ActivationFrameStack.java
+++ src/main/java/com/juliar/interpreter/ActivationFrameStack.java
@@ -8,17 +8,17 @@ public class ActivationFrameStack {
     private Stack<ActivationFrame> activationFrameStack = new Stack<>();
 
     public void push( ActivationFrame frame){
-        //Logger.log ("push [" + frame.frameName + "]");
-
-        if ( activationFrameStack.stream().filter( p -> p.frameName.equals( frame.frameName) ).count() == 1) {
+        Logger.log ("push [" + frame.frameName + "]");
+        /*
+        if ( activationFrameStack.stream().filter( p -> p.frameName.equals( "main" ) ).count() == 1) {
             throw new RuntimeException( "possible recursion on main method" );
-        }
+        }*/
 
         activationFrameStack.push( frame );
     }
 
     public ActivationFrame pop(){
-        //Logger.log ("pop [" + activationFrameStack.peek().frameName +"]");
+        Logger.log ("pop [" + activationFrameStack.peek().frameName +"]");
         return activationFrameStack.pop();
     }
 
